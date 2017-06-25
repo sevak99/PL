a5d2424ffbb50f50ca44d7fe9b780b8409f0df68
Sevak Abrahamyan <sevak_abrahamyan_99@mail.ru>
2017-06-25 11:35:08 +0400

    [+] Added 2 fragments: About and ProductList

--- app/src/main/java/com/abrahamyan/pl/ui/activity/MainActivity.java
+++ app/src/main/java/com/abrahamyan/pl/ui/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,6 +12,9 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.abrahamyan.pl.R;
+import com.abrahamyan.pl.ui.fragment.AboutFragment;
+import com.abrahamyan.pl.ui.fragment.ProductListFragment;
+import com.abrahamyan.pl.util.FragmentTransactionManager;
 
 public class MainActivity extends  BaseActivity  implements View.OnClickListener,
         NavigationView.OnNavigationItemSelectedListener {
@@ -47,7 +51,11 @@ public class MainActivity extends  BaseActivity  implements View.OnClickListener
         setListeners();
         customizeActionBar();
         initDrawer();
-        openScreen();
+        openScreen(
+                ProductListFragment.newInstance(),
+                R.id.nav_product_list,
+                false
+        );
     }
 
     @Override
@@ -80,13 +88,21 @@ public class MainActivity extends  BaseActivity  implements View.OnClickListener
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.nav_one:
+            case R.id.nav_product_list:
+                openScreen(
+                        ProductListFragment.newInstance(),
+                        R.id.nav_product_list,
+                        false
+                );
                 break;
 
-            case R.id.nav_two:
-                break;
+            case R.id.nav_about:
+                openScreen(
+                        AboutFragment.newInstance(),
+                        R.id.nav_about,
+                        true
+                );
 
-            case R.id.nav_three:
                 break;
         }
 
@@ -131,8 +147,15 @@ public class MainActivity extends  BaseActivity  implements View.OnClickListener
         actionBarDrawerToggle.syncState();
     }
 
-    private void openScreen() {
-        mNavigationView.getMenu().findItem(R.id.nav_one).setChecked(true);
+    private void openScreen(Fragment fragment, int item, boolean mustAddToBackStack) {
+        mNavigationView.getMenu().findItem(item).setChecked(true);
+
+        FragmentTransactionManager.displayFragment(
+                getSupportFragmentManager(),
+                fragment,
+                R.id.fl_main_container,
+                mustAddToBackStack
+        );
     }
 
     // ===========================================================
---
+++ app/src/main/java/com/abrahamyan/pl/ui/fragment/BaseFragment.java
@@ -0,0 +1,55 @@
+package com.abrahamyan.pl.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+import com.abrahamyan.pl.ui.activity.BaseActivity;
+
+public class BaseFragment extends Fragment {
+
+    // ===========================================================
+    // Constants
+
+    // ===========================================================
+    // ===========================================================
+    // Fields
+
+    // ===========================================================
+    // ===========================================================
+    // Constructors
+
+    // ===========================================================
+    // ===========================================================
+    // Getter & Setter
+
+    // ===========================================================
+    // ===========================================================
+    // Methods for/from SuperClass
+    // ===========================================================
+
+    // ===========================================================
+    // Listeners, methods for/from Interfaces
+    // ===========================================================
+
+    // ===========================================================
+    // Methods
+    // ===========================================================
+
+    protected void hideActionBarIcon() {
+        ((BaseActivity) getActivity()).hideActionBarIcon();
+    }
+
+    protected void showActionBarIcon() {
+        ((BaseActivity) getActivity()).showActionBarIcon();
+    }
+
+    protected void setActionBarIcon() {
+        ((BaseActivity) getActivity()).hideActionBarIcon();
+    }
+
+    protected void setActionBarTitle(String actionBarTitle) {
+        ((BaseActivity) getActivity()).setActionBarTitle(actionBarTitle);
+    }
+    // ===========================================================
+    // Inner and Anonymous Classes
+    // ===========================================================
+}
